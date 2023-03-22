e233d38ed9138bc64d1e7cdaefa6dd07b8013a67
jinpengchina <jinpengchina@gmail.com>
2023-03-21 23:21:28 -0400

    Update dddqn_other.py

--- dddqn_other.py
+++ dddqn_other.py
@@ -48,6 +48,26 @@ from plotly.offline import init_notebook_mode, iplot, iplot_mpl
 import dddqn.dddqn_other
 
 # test test
+
+def plot_train_test(train, test, date_split):
+    
+    data = [
+        Candlestick(x=train.index, open=train['Open'], high=train['High'], low=train['Low'], close=train['Close'], name='train'),
+        Candlestick(x=test.index, open=test['Open'], high=test['High'], low=test['Low'], close=test['Close'], name='test')
+    ]
+    layout = {
+         'shapes': [
+             {'x0': date_split, 'x1': date_split, 'y0': 0, 'y1': 1, 'xref': 'x', 'yref': 'paper', 'line': {'color': 'rgb(0,0,0)', 'width': 1}}
+         ],
+        'annotations': [
+            {'x': date_split, 'y': 1.0, 'xref': 'x', 'yref': 'paper', 'showarrow': False, 'xanchor': 'left', 'text': ' test data'},
+            {'x': date_split, 'y': 1.0, 'xref': 'x', 'yref': 'paper', 'showarrow': False, 'xanchor': 'right', 'text': 'train data '}
+        ]
+    }
+    figure = Figure(data=data, layout=layout)
+    iplot(figure)
+    
+
 def plot_train_test_by_q(train_env, test_env, Q, algorithm_name):
     
     # train
